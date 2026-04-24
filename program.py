f7192de558409acd9b026d5037ad782e8a74d218
TiredBW <avilovalesha@gmail.com>
2026-04-24 19:13:03 +0700

    first commit

---
+++ program.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+window = Tk() # creating window
+
+window.title("Calculator") # naming window
+window.geometry("500x500") # set window's resolution
+
+window.update_idletasks()
+window.resizable(False, False)
+window.mainloop()
